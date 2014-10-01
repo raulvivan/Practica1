cf777700f4ce86bd06c285b15206e0c6b3db791a
raulvivan <raul.vivanco@opendeusto.es>
2014-10-01 09:23:46 +0200

    CD2

--- src/main/java/main/Coche.java
+++ src/main/java/main/Coche.java
@@ -2,11 +2,43 @@ package main;
 
 public class Coche {
     private String marca;
-    
+
     private String modelo;
-    
+
     private String color;
-    
+
     private int velocidadMaxima;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
 
 }
